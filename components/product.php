ba1d012a61debfe01e9dc05639aef1083b841eb4
alinamukhametova <alinusiy10@bk.ru>
2022-12-14 23:39:51 +0300

    fix design

--- components/product.php
+++ components/product.php
@@ -2,9 +2,4 @@
     <img src="<?php echo $url; ?>" />
     <h3><?php echo $name; ?></h3>
     <span><?php echo $cost; ?> RUB</span>
-    <div class="product-colors">
-        <div class="product-color"></div>
-        <div class="product-color"></div>
-        <div class="product-color"></div>
-    </div>
 </a>
